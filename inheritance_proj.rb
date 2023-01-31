72480e2ecd3e9534f5dd0d95d838e1b704f937da
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-01-31 15:39:44 -0500

    final

--- inheritance_proj.rb
+++ inheritance_proj.rb
@@ -1,3 +1,45 @@
 class Employee
-  
+  attr_reader :name, :title, :salary, :boss
+
+   def initialize(name, title, salary, boss = nil)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+    self.add_boss()
+   end
+
+   def bonus_multiplier(multiplier)
+      bonus = @salary * multiplier
+      @bonus = bonus
+   end
+
+   def add_boss()
+    if @boss != nil
+      @boss.employees << self
+    end
+   end
+end
+
+class Manager < Employee
+  def initialize(name, title, salary, boss = nil)
+    super
+    @employees = []
+    @employees_salary = []
+   end
+
+   def employees
+    @employees
+   end
+   
+   def employees_salary
+    @employees_salary
+   end
+
+   def bonus(multiplier)
+    self.employees.each do |employee|
+      @employees_salary << employee.salary
+    end
+    @employees_salary.sum * multiplier
+    end
 end
