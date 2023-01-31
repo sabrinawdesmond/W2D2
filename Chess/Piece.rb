3f10fb8f6d823d40d6e0c933eeb8e29ab7a397b8
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-01-31 16:41:22 -0500

    Piece.rb

--- Chess/Piece.rb
+++ Chess/Piece.rb
@@ -1,7 +1,36 @@
 class Piece
-    def initialize
+    require_relative "Board"
 
+    def initialize(color, board, position)
+        @color = symbol
+        @board = Board
+        @position = []
     end
+
+    def to_s
+    
+    end
+
+    def empty?
+    
+    end
+
+    def valid_moves
+    
+    end
+
+    def pos=(value)
+    
+    end
+
+    def symbol
+    
+    end
+
+    def move_into_check?(end_pos)
+    
+    end
+
 
 
 end
@@ -19,3 +48,80 @@ class Queen < Piece
         
     end
 end
+
+class King < Piece
+
+    def symbol_wh
+    @symbol = ♔
+    end
+
+    def symbol_blk
+    @symbol = ♚
+    end
+
+    def move_dirs
+    
+    end
+end
+
+class Rook < Piece
+    def symbol_wh
+        @symbol = ♖
+    end
+    
+    def symbol_blk
+        @symbol = ♜
+    end
+
+    def move_dirs
+        
+    end
+end
+
+class Bishop < Piece
+    def symbol_wh
+        @symbol = ♗
+    end
+    
+    def symbol_blk
+        @symbol = ♝
+    end
+
+    def move_dirs
+        
+    end
+end
+
+class Knight < Piece
+    def symbol_wh
+        @symbol = ♘
+    end
+    
+    def symbol_blk
+        @symbol = ♞
+    end
+
+    def move_dirs
+        
+    end
+end
+
+class Pawn < Piece
+    def symbol_wh
+        @symbol = ♙
+    end
+    
+    def symbol_blk
+        @symbol = ♟
+    end
+
+    def move_dirs
+        
+    end
+end
+
+class NullPiece < Piece
+    
+end
+
+
