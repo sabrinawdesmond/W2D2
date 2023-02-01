e29bebd3b7e941688c100ca8e222a107b083c325
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-01 13:42:48 -0500

    Slideable

---
+++ Chess/Piece/slideable.rb
@@ -0,0 +1,19 @@
+module Slideable
+
+    HORIZONTAL_DIRS = [
+        [-1,0],
+        [0,-1],
+        [1,0],
+        [0,1]
+].freeze 
+    end
+
+    DIAGONAL_DIRS = [
+        [-1,-1]
+        [-1,1]
+        [1,1]
+        [1,-1]
+].freeze
+    end
+
+end
